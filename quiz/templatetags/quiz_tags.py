d274b578310c1488a56934ebeae4df2b0c6d98c5
Chris Chambers <magma.chambers@gmail.com>
2010-10-01 14:01:18 +0100

    Adding template filter: user|quiz_taken:quiz ==> bool

--- quiz/templatetags/quiz_tags.py
+++ quiz/templatetags/quiz_tags.py
@@ -1,4 +1,8 @@
 from django import template
+
+from django.contrib.auth.models import User
+from quiz.models import QuizResult, Quiz
+
 register = template.Library()
 
 @register.filter(name='percentage')
@@ -8,3 +12,9 @@ def percentage(part, population):
     except (ValueError, ZeroDivisionError):
         return ''
 
+@register.filter(name='quiz_taken')
+def quiz_taken(user, quiz):
+    if (not isinstance(user, User) or
+        not isinstance(quiz, Quiz)):
+        return ''
+    return QuizResult.objects.filter(user=user, quiz=quiz).exists()
--- quiz/tests/templatetags.py
+++ quiz/tests/templatetags.py
@@ -1,7 +1,10 @@
 from nose.tools import *
-from unittest import TestCase
 
-from quiz.templatetags.quiz_tags import percentage
+from django.contrib.auth.models import User
+from django.test import TestCase
+from quiz.models import Quiz, QuizResult
+
+from quiz.templatetags.quiz_tags import percentage, quiz_taken
 
 
 class TestPercentageFilter(TestCase):
@@ -20,4 +23,32 @@ class TestPercentageFilter(TestCase):
 
         for part, population, expected in data:
             assert_equal(expected, percentage(part, population))
+
+
+class TestQuizTakenFilter(TestCase):
+    fixtures = ['python-zen.yaml', 'testuser.yaml']
+    # 1 quiz, 9 questions, 26 answers, 9 correct answers (one per question)
+    # A single test-user, TestyMcTesterson, with password 'password'
+
+    def setUp(self):
+        super(TestQuizTakenFilter, self).setUp()
+        self.quiz = Quiz.objects.get(slug='python-zen')
+        self.user = User.objects.get(username='TestyMcTesterson')
+
+    def test_quiz_taken_with_valid_inputs(self):
+        assert quiz_taken(self.user, self.quiz) is False
+        result = QuizResult.objects.create(
+            user=self.user,
+            quiz=self.quiz,
+            score=9,
+            maximum_score=9
+        )
+        assert quiz_taken(self.user, self.quiz) is True
+        result.delete()
+        assert quiz_taken(self.user, self.quiz) is False
+
+    def test_quiz_taken_with_invalid_inputs(self):
+        assert_equal('', quiz_taken('foo', self.quiz))
+        assert_equal('', quiz_taken(self.user, 'foo'))
+        assert_equal('', quiz_taken(1, 2))
 
